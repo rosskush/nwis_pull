a39f84bca8b02996633a5e1787fdb161fa6226a6
rosskush <rosskushnereit@Rosss-MacBook-Air.local>
2018-04-20 08:26:02 -0500

    attempted to add setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import sys
 from distutils.core import setup
+# from setuptools import setup
 
 DESCRIPTION = """\
 Classes for pulling nwis data.
 """
+
+# setup(name="nwis_pull",version="0.1",description="Classes for pulling nwis data into pandas dataframes",
+	# author="Ross Kushnereit",packages=["nwis_pull"])
+
 
 def run():
     setup(name="nwis_pull",
